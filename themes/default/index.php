770dfef72dce5f9c2534385139218ddb48460868
DonaldAlbert <nomail@example.com>
2015-02-10 20:30:39 +0200

    starting implementation of the theme engine's "blocks" feature.

--- themes/default/index.php
+++ themes/default/index.php
@@ -4,8 +4,8 @@
     <head>
         <title>Demo default theme</title>
 <?php
-    foreach( $theme_content['theme']['css'] as $css ) {
-        $css = $theme_content['theme']['root'] . '/' . $css;
+    foreach( $tc->content['theme']['css'] as $css ) {
+        $css = $tc->content['theme']['root'] . '/' . $css;
 ?>
         <link rel="stylesheet" type="text/css" href="<?= $css; ?>" media="screen">
 <?php
@@ -13,9 +13,9 @@
 ?>
     </head>
     <body>
-        <h1>Hello World<small>just testing</small></h1>
+        <h1>Hello World<small>index</small></h1>
 
-        <h2><?= $theme_content['content']['h2']; ?></h2>
+        <h2><?= $tc->content['content']['h2']; ?></h2>
 
         <footer><?php include('partials/footer.php'); ?></footer>
     </body>
